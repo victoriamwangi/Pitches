467c1afb9f54fd657e40a74ba1c584040e304a0a
victoria mwangi <victoriamwangi021@gmail.com>
2022-05-11 16:05:52 +0300

    test if gitingore works

--- config.py
+++ config.py
@@ -4,6 +4,11 @@ class Config():
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:4798@localhost/pitch'
     SECRET_KEY = os.environ.get('SECRET_KEY')
     UPLOADED_PHOTOS_DEST ='app/static/photos'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 class DevConfig(Config):
     DEBUG = True
--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-export SECRET_KEY='TTTTT'
+export SECRET_KEY='TTTTTT'
 python3.9 manage.py server
