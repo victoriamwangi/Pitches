b5f3c0d14610edb1c2e6e52ab9bc74d7c194def3
victoria mwangi <victoriamwangi021@gmail.com>
2022-05-12 12:04:23 +0300

    remove .pyc files

--- app/email.py
+++ app/email.py
@@ -3,7 +3,7 @@ from flask import render_template
 from . import mail
 
 def mail_message(subject,template,to,**kwargs):
-    sender_email = 'victoriamwangi021@gmail.com'
+    sender_email = 'victoria.mwangi@student.moringaschool.com'
 
     email = Message(subject, sender=sender_email, recipients=[to])
     email.body= render_template(template + ".txt",**kwargs)
